6498366bca6cd3b9f26dc51afb5f8cb08f6ad433
drathnis <drathnis@gmail.com>
2018-03-26 19:56:57 -0400

    Update 4x4_script.js

--- static/js/4x4_script.js
+++ static/js/4x4_script.js
@@ -27,12 +27,13 @@
         bl = $('[name=colpick_BL]').val();
         br = $('[name=colpick_BR]').val();
         $.ajax({
-            url: 'http://24.194.15.104/set4x4',
+            url: "http://24.194.15.104/set4x4",
             type: 'POST',
             data: {
                 tl: tl, tr:tr, bl:bl, br:br
             }
         });
+        alert("Done");
     }
     function RUNATPAGELOAD(){
         var ttt = document.getElementsByClassName('input-color-value');
