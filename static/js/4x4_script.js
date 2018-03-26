afeba7d7d5b0c65501cbc0b9f50e59789e2a4bc5
drathnis <drathnis@gmail.com>
2018-03-26 19:00:11 -0400

    First Upload

---
+++ static/js/4x4_script.js
@@ -0,0 +1,43 @@
+    function ColorChanged() {
+        var Color = $(this).val()
+        var id = this.id;
+        var info=["0","0","0","0"]
+        var tl,tr,bl,br;
+        if (this.id == "cpick_T_id"){
+            $('[name=colpick_ALL]').val("#000000")
+            $('[name=colpick_TL]').val($(this).val())
+            $('[name=colpick_TR]').val($(this).val())
+        }
+        else if(this.id == "cpick_B_id"){
+            $('[name=colpick_ALL]').val("#000000")
+            $('[name=colpick_BL]').val($(this).val())
+            $('[name=colpick_BR]').val($(this).val())
+        }
+        else if(this.id == "cpick_ALL_id"){
+
+            $('[name=colpick_T]').val($(this).val())
+            $('[name=colpick_B]').val($(this).val())
+            $('[name=colpick_TL]').val($(this).val())
+            $('[name=colpick_TR]').val($(this).val())
+            $('[name=colpick_BL]').val($(this).val())
+            $('[name=colpick_BR]').val($(this).val())
+        }
+        tl = $('[name=colpick_TL]').val();
+        tr = $('[name=colpick_TR]').val();
+        bl = $('[name=colpick_BL]').val();
+        br = $('[name=colpick_BR]').val();
+        $.ajax({
+            url: '/set4x4',
+            type: 'POST',
+            data: {
+                tl: tl, tr:tr, bl:bl, br:br
+            }
+        });
+    }
+    function RUNATPAGELOAD(){
+        var ttt = document.getElementsByClassName('input-color-value');
+        for (i=0;i<ttt.length;i++){
+            ttt[i].addEventListener('change', ColorChanged, false);
+        }
+    }
+    window.onload = RUNATPAGELOAD;
